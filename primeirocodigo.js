6c9f760e074f4ece66e9599b66a7797ff9c5fd18
Bentobento <bentoagostini5@gmail.com>
2024-03-04 11:04:59 -0300

    nah i'd win

--- primeirocodigo.js
+++ primeirocodigo.js
@@ -1,1 +1,9 @@
-console.log("la ele");
+function compare(x) {
+    if (x < 5) {
+        alert("menor que 5");
+    } else if (x == 5) {
+        alert("igual a 5");
+    } else {
+        alert("maior que 5");
+    }
+}
