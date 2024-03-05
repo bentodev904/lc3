60c5307e82d82a56191c20522207a0564f6cb4b1
bentobento <bentoagostini5@gmail.com>
2024-03-05 10:12:49 -0300

    waaa

--- javascript/primeirocodigo.js
+++ javascript/primeirocodigo.js
@@ -1,9 +1,12 @@
+x=+prompt("roier pode falar o numero bbg")
 function compare(x) {
-    if (x < 5) {
-        alert("menor que 5");
-    } else if (x == 5) {
-        alert("igual a 5");
-    } else {
-        alert("maior que 5");
+    if(x < 5){
+        alert("seu numero é menor que 5... :(");
+    }else if(x == 5){
+        alert("seu numero é igual a 5!! >:P");
+    }else if(x > 5){
+        alert("Maior que 5!!! Iepiiiiiiiiiiiii OwO");
+    }else if(x = String){
+        alert("é número >:(")
     }
 }
