ab54d2955e394c1ae3db1dd9bfbdadf95dfa180f
bentbento <bentoagostini5@gmail.com>
2024-03-12 11:19:07 -0300

    aaaaa

--- javascript/primeirocodigo.js
+++ javascript/primeirocodigo.js
@@ -1,11 +1,8 @@
-function compare(x) {
-    if(x < 5){
-        alert("seu numero é menor que 5... :(");
-    }else if(x == 5){
-        alert("seu numero é igual a 5!! >:P");
-    }else if(x > 5){
-        alert("Maior que 5!!! Iepiiiiiiiiiiiii OwO");
-    }else if(x = String){
-        alert("é número >:(")
-    }
+function compare(x, y) {
+    alert("-" + x + "-");
+    alert("-" + y + "-");
+    soma = x + y;
+    mult = x*y;
+    div = x/y;
+    document.write("Soma = " +soma +'' +"Multiplicacao = "+mult +'' +"Divisao = "+div +'')
 }
