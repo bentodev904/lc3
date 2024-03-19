aad05541100c02d31048c4e9c1be390bfe541569
bento <bentoagostini5@gmail.com>
2024-03-19 11:22:05 -0300

    aaaaaaaaaaaaaa

--- javascript/calcular.js
+++ javascript/calcular.js
@@ -1,5 +1,9 @@
 function maths(x, y) {
     y = (document.getElementById('num2').value)
+    if (x === '' || y === '') {
+        alert('O campo está vazio!');
+        return false;
+    }
     soma = parseFloat(x) + parseFloat(y);
     mult = x * y;
     div = x / y;
---
+++ javascript/nomeeafins.js
@@ -0,0 +1,16 @@
+function testname(x){
+    if (x === '') {
+        alert('O campo está vazio!');
+        return false;
+    }
+    document.getElementById('insira').innerHTML = "Confirmar?"
+    document.getElementById('yes').value = "Sim"
+    document.getElementById('no').value = "Não"
+    document.getElementById('yes').style = "background-color:green;border-radius:3px" 
+    document.getElementById('no').style = "background-color:red;border-radius:3px"
+}
+
+function yes(x){
+    save = x;
+    document.getElementById('salvar').value =  save
+}
